82ba19907b6a633ee71363cf999293c33bc7e982
Henning Hall <henning.hall@hotmail.com>
2015-12-13 21:46:02 +0100

    Lab 1 Step 3 done

--- client.js
+++ client.js
@@ -4,7 +4,19 @@ displayView = function(viewId){
 }
 
 window.onload = function(){
-displayView("welcomeView");
-//code that is executed as the page is loaded.
-//You shall put your own custom code here.
+    displayView("welcomeView");
+}
+
+function validateSignUpForm() {
+    // Passwords must be equal
+    var password1 = document.forms["signUpForm"]["password1"].value;
+    var password2 = document.forms["signUpForm"]["password2"].value;
+    if (password1 != password2) {
+        alert("Password must be the same");
+        return false;
+    }
+    if (password1.length < 8){
+        alert("Password must be at least 8 characters.");
+        return false;
+    }
 }
